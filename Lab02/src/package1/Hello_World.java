a504c833da7d908a1ef25122572dd1a8d6b151b1
Javier Aburto <jaburto@casapellas.com>
2024-08-29 10:19:14 -0600

    Primer Commit, Laboratorio 29 Ago 2024

---
+++ Lab02/src/package1/Hello_World.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+		System.out.println("Primer commit");
+
+		
+	}
+
+}
