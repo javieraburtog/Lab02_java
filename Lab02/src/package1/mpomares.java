9a77950e27e9585f546d4b6c836450bd3f517286
marpo89 <miltonpomares_13@hotmail.com>
2024-08-29 12:54:42 -0400

    Add new class

---
+++ Lab02/src/package1/mpomares.java
@@ -0,0 +1,17 @@
+package package1;
+
+public class mpomares {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Resultado de la suma: "+ suma(5,10));
+		
+	}
+	
+	public static int suma (int a, int b)
+	{
+		
+		return a + b;
+		
+	}
+}
