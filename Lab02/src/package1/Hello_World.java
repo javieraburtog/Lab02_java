34877e1935b45b7f617a5c5d252821efbf7fd0d6
Javier Aburto <jaburto@casapellas.com>
2024-08-29 14:07:57 -0600

    Prueba de edición de la misma linea: Hello World

--- Lab02/src/package1/Hello_World.java
+++ Lab02/src/package1/Hello_World.java
@@ -5,7 +5,7 @@ public class Hello_World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
+		System.out.println("Hello World - Javier");
 		
 		System.out.println("Primer commit");
 
